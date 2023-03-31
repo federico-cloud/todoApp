db753ffc589f6d789d1c1a05f13554e6ccd44596
Federico Galan <fegv.2@hotmail.com.ar>
2023-03-31 15:42:33 -0300

    HTML/CSS/JS: Initial structure

---
+++ src/todos/app.js
@@ -0,0 +1,14 @@
+import html from './app.html?raw';
+
+export const App = (elementId) => {
+
+    (() =>{
+
+        const app = document.createElement('div');
+        app.innerHTML = html;
+
+        document.querySelector(elementId).appendChild(app);
+
+    })();
+
+}
