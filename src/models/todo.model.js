876e6378f7243b385a6e4e0aadf93bde35177844
Federico Galan <fegv.2@hotmail.com.ar>
2023-04-01 11:54:31 -0300

    JS: Class Todo was modified, the uuid was added

--- src/models/todo.model.js
+++ src/models/todo.model.js
@@ -1,7 +1,9 @@
+import {v4 as uuid} from 'uuid'
+
 export class Todo {
 
     constructor(desc){
-        this.id        = 1;
+        this.id        = uuid();
         this.desc      = desc;
         this.done      = false;
         this.createdAt = new Date();
