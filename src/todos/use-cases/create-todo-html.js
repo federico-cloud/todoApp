3b59dc2a335b2deca833dd6b2034a1aef07ca7eb
Federico Galan <fegv.2@hotmail.com.ar>
2023-04-06 15:58:39 -0300

    JS: render-todo and create-todo-html.js was modified

--- src/todos/use-cases/create-todo-html.js
+++ src/todos/use-cases/create-todo-html.js
@@ -2,10 +2,21 @@ export const createTodoHTML = (todo) =>{
 
     if(!todo) throw new Error("Todo object is required");
 
-    const html      = `<h1>${todo.desc}</h1>`;
-    const liElement = document.createElement('li');
+    const html      = `
+            <div class="view">
+                <input class="toggle" type="checkbox" checked>
+                <label>${todo.desc}</label>
+                <button class="destroy"></button>
+            </div>
+            <input class="edit" value="Create a TodoMVC template">
+    `;
 
+    const liElement = document.createElement('li');
     liElement.innerHTML = html;
+    liElement.setAttribute('data-id', todo.id);
+    
+    if(todo.done) liElement.classList.add('completed');
+
 
     return liElement;
 
